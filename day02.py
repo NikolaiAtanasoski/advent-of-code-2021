9a66dd9bc8dd8a271e1d0e111d8211c90dc58820
Nikolai Atanasoski <nikolai.atanasoski@compax.at>
2021-12-04 14:23:34 +0100

    day02 part 2

---
+++ day02.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+
+def load_input(file_name):
+    with open(file_name, "r") as file:
+        return file.read().splitlines()
+
+
+filename = f"{Path(__file__).stem}.input"
+instructions = load_input(filename)
+print(instructions)
+
+aim = 0
+horizontal = 0
+depth = 0
+
+for command in instructions:
+    print(command)
+    name = command.split(" ")[0]
+    value = command.split(" ")[1]
+
+    if name == "down":
+        aim += int(value)
+    elif name == "up":
+        aim -= int(value)
+    elif name == "forward":
+        horizontal += int(value)
+        depth += (aim * int(value))
+    else:
+        print(f"unknown command found: {command}")
+
+result = horizontal * depth
+print(f"result : {result}")
